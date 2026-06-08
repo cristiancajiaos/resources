5284c7c6fe604c8ac59f09e2992e3ac42a948b64
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-06-08 17:54:40 -0400

    Creado servicio TodoService; inyectado en vista Todos

--- src/app/modules/todos/todos.ts
+++ src/app/modules/todos/todos.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { TodoService } from '../../services/todo-service';
+import { Todo } from '../../interfaces/todo';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-todos',
@@ -6,4 +9,35 @@ import { Component } from '@angular/core';
   templateUrl: './todos.html',
   styleUrl: './todos.scss',
 })
-export class Todos {}
+export class Todos implements OnInit, OnDestroy {
+
+  private todoService = inject(TodoService);
+
+  public todos: Todo[] = [];
+
+  private getTodosSubscription: Subscription;
+
+  ngOnInit(): void {
+    this.getTodos();
+  }
+
+  private getTodos(): void {
+    this.todoService.getTodos().subscribe({
+      next: (todos) => {
+        this.todos = todos;
+      },
+      error: (error) => {
+
+      },
+      complete: () => {
+
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.getTodosSubscription) {
+      this.getTodosSubscription.unsubscribe();
+    }
+  }
+}
