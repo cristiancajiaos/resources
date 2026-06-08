abdd2b449f5e7f8550b46ad6f4852fadc4ece37c
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-06-08 17:47:30 -0400

    Creada vista de Todos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { Posts } from './modules/posts/posts';
 import { Comments } from './modules/comments/comments';
 import { Albums } from './modules/albums/albums';
 import { Photos } from './modules/photos/photos';
+import { Todos } from './modules/todos/todos';
 
 export const routes: Routes = [
   { path: '', component: Home },
@@ -11,5 +12,6 @@ export const routes: Routes = [
   { path: 'comments', component: Comments },
   { path: 'albums', component: Albums},
   { path: 'photos', component: Photos},
+  { path: 'todos', component: Todos},
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
